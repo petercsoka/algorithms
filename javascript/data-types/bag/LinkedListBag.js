071eb362c5dc28a56b624e18e22c8a03e7f74090
Peter Csoka <petercsoka@gmail.com>
2016-09-09 19:09:48 +0100

    Populat Linked List Bag functions

--- javascript/data-types/bag/LinkedListBag.js
+++ javascript/data-types/bag/LinkedListBag.js
@@ -1,25 +1,36 @@
 function Node()
 {
-    
+    this._item = null;
+    this._next = null;
 }
 
 function LinkedListBag(){
-
+    this._first = null;
+    this._size = 0;
 }
 
 LinkedListBag.prototype.add = function(item)
 {
-
+    var oldfirst = this._first;
+    this._first = new Node();
+    this._first._item = item;
+    this._first._next = oldfirst;
+    this._size++;
 }
 
 LinkedListBag.prototype.isEmpty = function()
 {
-
+    if (!this.isEmpty()) {
+        var item = this._first;
+        this._first = this._first._next;
+        this._size--;
+        return item;
+    }
 }
 
 LinkedListBag.prototype.size = function()
 {
-
+    return this._size;
 }
 
 /**
@@ -31,5 +42,13 @@ LinkedListBag.prototype.size = function()
  * }
  */
 LinkedListBag.prototype[Symbol.iterator] = function(){
-
+    var current = this._first;
+    return {
+        next: function(){
+            var done = current._next == null;
+            var value = current._item;
+            current = current._next;
+            return { value, done };
+        }
+    };
 }

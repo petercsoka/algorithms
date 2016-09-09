30ed363c9bc96d67a93c1b078a73749ab4af4fac
Peter Csoka <petercsoka@gmail.com>
2016-09-09 22:35:01 +0100

    Init tree data type

---
+++ javascript/data-types/tree/Tree.js
@@ -0,0 +1,32 @@
+function Node(data)
+{
+    this._data = data;
+    this._children = [];
+}
+
+function Tree()
+{
+    this.root = null;
+}
+
+Tree.prototype.add = function()
+{
+
+}
+
+Tree.prototype.remove = function()
+{
+
+}
+
+Tree.prototype.container = function()
+{
+
+}
+
+Tree.prototype[Symbol.iterator] = function()
+{
+
+}
+
+module.exports = Tree;

52f668a0eeec9a9e5edd5b1745cda73a6e771074
Peter Csoka <petercsoka@gmail.com>
2016-09-09 18:00:24 +0100

    Populate bag functions

--- javascript/data-types/bag/Bag.js
+++ javascript/data-types/bag/Bag.js
@@ -1,20 +1,39 @@
 function Bag(){
-
+    this.bag = [];
 }
 
 Bag.prototype.add = function(item)
 {
-
+    this.bag.push(item);
 }
 
 Bag.prototype.isEmpty = function()
 {
-
+    return this.bag.length === 0;
 }
 
 Bag.prototype.size = function()
 {
-
+    return this.bag.length;
 }
 
-Bag.prototype.
+/**
+ * Add custom iterator for easily iterate thorugh the items in the bag
+ *
+ * Usage:
+ * for(var item of bag) {
+ *     console.log(item);
+ * }
+ */
+Bag.prototype[Symbol.iterator] = function(){
+    var self = this;
+    var index = 0;
+    return {
+        next: function(){
+            var value = self.bag[index];
+            var done = index >= self.size();
+            index++;
+            return { value, done };
+        }
+    };
+}

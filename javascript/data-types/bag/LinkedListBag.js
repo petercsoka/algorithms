89c7ca6d6e69de8505752441eb89759c6eb28a90
Peter Csoka <petercsoka@gmail.com>
2016-09-09 19:07:16 +0100

    Init Linked List Bag data type

---
+++ javascript/data-types/bag/LinkedListBag.js
@@ -0,0 +1,35 @@
+function Node()
+{
+    
+}
+
+function LinkedListBag(){
+
+}
+
+LinkedListBag.prototype.add = function(item)
+{
+
+}
+
+LinkedListBag.prototype.isEmpty = function()
+{
+
+}
+
+LinkedListBag.prototype.size = function()
+{
+
+}
+
+/**
+ * Add custom iterator for easily iterate thorugh the items in the bag
+ *
+ * Usage:
+ * for(var item of bag) {
+ *     console.log(item);
+ * }
+ */
+LinkedListBag.prototype[Symbol.iterator] = function(){
+
+}

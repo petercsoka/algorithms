787450d5fb3ddce6631896785f25cae5988f328e
Peter Csoka <petercsoka@gmail.com>
2016-09-09 22:01:43 +0100

    Move bag specs into one spec

--- javascript/spec/data-types/bag/BagSpec.js
+++ javascript/spec/data-types/bag/BagSpec.js
@@ -1,64 +1,74 @@
-var Bag = require("../../../data-types/bag/Bag.js")
+var Bag = require("../../../data-types/bag/Bag.js");
+var LinkedListBag = require("../../../data-types/bag/LinkedListBag.js");
 var Storage;
 
 describe("Bag DT", function() {
 
-    beforeEach(function() {
-        Storage = new Bag();
+        function BagTests(BagDataType){
+        beforeEach(function() {
+            Storage = new Bag();
+        });
+
+        it("should have add method", function(){
+            expect(typeof Storage.add).toEqual('function');
+        });
+
+        it("should not have a remove or pop method", function(){
+            expect(typeof Storage.remove).toEqual('undefined');
+            expect(typeof Storage.pop).toEqual('undefined');
+        });
+
+        it("should have isEmpty method", function(){
+            expect(typeof Storage.isEmpty).toEqual('function');
+        });
+
+        it("should have size method", function(){
+            expect(typeof Storage.size).toEqual('function');
+        });
+
+        it("should be able to add item to the bag", function(){
+            Storage.add("Hello");
+            expect(Storage.size()).toEqual(1);
+            expect(Storage.isEmpty()).toBe(false);
+            expect(Storage.bag[0]).toBe("Hello");
+        });
+
+        it("should be return the right bag size", function(){
+            Storage.add("a");
+            Storage.add("b");
+            Storage.add("c");
+            Storage.add("d");
+            Storage.add("e");
+            Storage.add("f");
+            expect(Storage.size()).toEqual(6);
+        });
+
+        it("should be detect if the bag is empty", function(){
+            expect(Storage.isEmpty()).toBe(true);
+        });
+
+        it("should be detect if the bag is not empty", function(){
+            Storage.add("a");
+            expect(Storage.isEmpty()).toBe(false);
+        });
+
+        it("should be iterable", function(){
+            Storage.add(1);
+            Storage.add(2);
+            Storage.add(3);
+            var expected_item = 1;
+            for(var item of Storage) {
+                expect(item).toEqual(expected_item);
+                expected_item++;
+            }
+        });
+    }
+
+    describe("Normal Bag data type", function(){
+        BagTests(Bag);
     });
 
-    it("should have add method", function(){
-        expect(typeof Storage.add).toEqual('function');
+    describe("Linked List Bag data type", function(){
+        BagTests(LinkedListBag);
     });
-
-    it("should not have a remove or pop method", function(){
-        expect(typeof Storage.remove).toEqual('undefined');
-        expect(typeof Storage.pop).toEqual('undefined');
-    });
-
-    it("should have isEmpty method", function(){
-        expect(typeof Storage.isEmpty).toEqual('function');
-    });
-
-    it("should have size method", function(){
-        expect(typeof Storage.size).toEqual('function');
-    });
-
-    it("should be able to add item to the bag", function(){
-        Storage.add("Hello");
-        expect(Storage.size()).toEqual(1);
-        expect(Storage.isEmpty()).toBe(false);
-        expect(Storage.bag[0]).toBe("Hello");
-    });
-
-    it("should be return the right bag size", function(){
-        Storage.add("a");
-        Storage.add("b");
-        Storage.add("c");
-        Storage.add("d");
-        Storage.add("e");
-        Storage.add("f");
-        expect(Storage.size()).toEqual(6);
-    });
-
-    it("should be detect if the bag is empty", function(){
-        expect(Storage.isEmpty()).toBe(true);
-    });
-
-    it("should be detect if the bag is not empty", function(){
-        Storage.add("a");
-        expect(Storage.isEmpty()).toBe(false);
-    });
-
-    it("should be iterable", function(){
-        Storage.add(1);
-        Storage.add(2);
-        Storage.add(3);
-        var expected_item = 1;
-        for(var item of Storage) {
-            expect(item).toEqual(expected_item);
-            expected_item++;
-        }
-    });
-
 });

ceb5b13ca4d5c58c1819c9aa08d5724267d0b11b
Peter Csoka <petercsoka@gmail.com>
2016-09-09 18:05:24 +0100

    Init Stack data type

---
+++ javascript/data-types/stack/Stack.js
@@ -0,0 +1,19 @@
+function Stack(){
+
+}
+
+Stack.prototype.push = function(value) {
+
+};
+
+Stack.prototype.pop = function() {
+
+};
+
+Stack.prototype.size = function() {
+
+};
+
+Stack.prototype.isEmpty = function() {
+
+};

c1d55be5f11c55135ed2bec8e4188ac0f7e94f76
Peter Csoka <petercsoka@gmail.com>
2016-09-09 19:15:36 +0100

    Init Queue data type

---
+++ javascript/data-types/queue/Queue.js
@@ -0,0 +1,29 @@
+function Queue()
+{
+
+}
+
+Queue.prototype.enqueue = function(item)
+{
+
+}
+
+Queue.prototype.dequeue = function()
+{
+
+}
+
+Queue.prototype.isEmpty = function()
+{
+
+}
+
+Queue.prototype.size = function()
+{
+
+}
+
+Queue.prototype[Symbol.iterator] = function()
+{
+    
+}

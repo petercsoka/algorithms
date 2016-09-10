65accfc0d2c82e30d033ed0acb52617114b6f70d
Peter Csoka <petercsoka@gmail.com>
2016-09-10 17:49:15 +0100

    Populate tree functions

--- javascript/data-types/tree/Tree.js
+++ javascript/data-types/tree/Tree.js
@@ -1,3 +1,5 @@
+var Queue = require('../queue/Queue.js');
+
 function Node(data)
 {
     this._data = data;
@@ -9,20 +11,63 @@ function Tree()
     this.root = null;
 }
 
-Tree.prototype.add = function()
+Tree.prototype.add = function(data, toNodeData)
 {
-
+    var new_node = new Node(data);
+    var parent = toNodeData ? this.findTree(toNodeData) : null;
+    if (parent) {
+        parent._children.push(new_node);
+    } else {
+        if(!this.root) {
+            this.root = new_node;
+        } else {
+            return "Root node already assigned";
+        }
+    }
 }
 
-Tree.prototype.remove = function()
+Tree.prototype.remove = function(data)
 {
+    if (this.root.data == data) {
+        return this.root = null;
+    }
 
+    var queue = new Queue();
+    queue.enqueue(this.root);
+    while(!queue.isEmpty()) {
+        var node = queue.dequeue();
+        for(var i = 0; i < node._children.length; i++) {
+            if(node._children[i]._data === data) {
+                node._children.splice(i, 1);
+            } else {
+                queue.enqueue(node._children[i]);
+            }
+        }
+    }
 }
 
-Tree.prototype.container = function()
+Tree.prototype.contains = function(data)
 {
-
+    return this.findTree(data) ? true : false;
 }
+
+Tree.prototype.findTree = function(data) {
+    var queue = new Queue();
+    queue.enqueue(this.root);
+
+    while(!queue.isEmpty()) {
+        var node = queue.dequeue();
+
+        if(node._data === data) {
+            return node;
+        }
+
+        for(var i = 0; i < node._children.length; i++) {
+            queue.enqueue(node._children[i]);
+        }
+    }
+    return null;
+};
 
 Tree.prototype[Symbol.iterator] = function()
 {

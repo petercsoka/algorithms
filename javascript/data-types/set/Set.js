034b25d51540b243f8ffd3691366c4db258d9b34
Peter Csoka <petercsoka@gmail.com>
2016-09-11 16:02:05 +0100

    Add Set data type skeleton

---
+++ javascript/data-types/set/Set.js
@@ -0,0 +1,41 @@
+function Set()
+{
+
+}
+
+Set.prototype.add = function(item)
+{
+
+};
+
+Set.prototype.remove = function(item)
+{
+
+};
+
+Set.prototype.contains = function(item)
+{
+
+};
+
+Set.prototype.union = function(set)
+{
+
+};
+
+Set.prototype.intersect = function(set)
+{
+
+};
+
+Set.prototype.difference = function(set)
+{
+
+};
+
+Set.prototype.subset = function(set)
+{
+
+};
+
+module.exports = Set;
